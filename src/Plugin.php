fc3c1304425b4edd7dc42262fab18640ad54b42b
Tobias Liebig <work@etobi.de>
2021-02-04 14:04:24 +0100

    TASK fix code style

--- src/Plugin.php
+++ src/Plugin.php
@@ -53,11 +53,11 @@ class Plugin implements PluginInterface, Capable, EventSubscriberInterface
         return $this->scriptManager;
     }
 
-    public function deactivate(Composer $composer, IOInterface $io)
+    function deactivate(Composer $composer, IOInterface $io)
     {
     }
 
-    public function uninstall(Composer $composer, IOInterface $io)
+    function uninstall(Composer $composer, IOInterface $io)
     {
     }
 
